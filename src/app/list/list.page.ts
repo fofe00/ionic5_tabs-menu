e01294a1d8b9e430a284770d2e41781b518a6bf4
LONFO FOFE <b.fofe@yahoo.com>
2020-06-11 11:32:51 +0100

    addhttp request and get data from laravel app

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
 
-  constructor() { }
+
+  constructor(private http:HttpClient) {
+
+  }
 
   ngOnInit() {
+    //console.log("oninit");
+    this.getList();
   }
+  getList(){
+    this.http.get('http://127.0.0.1:8000/api/person').subscribe((response) => {
+      console.log(response['data']);
+    },error => {
+      console.log(error);
+    });
+  }
+
 
 }
